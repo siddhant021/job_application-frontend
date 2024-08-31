b5c6e26b878909811eaac653bd2f0b71c210bc10
Siddhant Kapoor <siddhantkapoor2025@gmail.com>
2024-08-31 23:19:57 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Login from './pages/login'
 import Home from './pages/Home'
 import 'react-toastify/dist/ReactToastify.css';
 import {ToastContainer} from "react-toastify"
 import Signup from './pages/Signup';
+import Login from './pages/Login';
 const App = () => {
   return (
        <Router>
